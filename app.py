d7fb4494b060d3fa8d071e357c137037d0a646e2
mats <mats.kazuki@gmail.com>
2023-04-15 04:01:35 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+
+from chalice import Chalice
+from feedgen.feed import FeedGenerator
+import requests
+
+INTERNAL_EVENT_URL = 'https://aws-startup-lofts.com/apj/api/session'
+EXTERNAL_EVENT_URL = 'https://aws-startup-lofts.com/apj/api/externalevent'
+
+app = Chalice(app_name='loft-events-feed')
+
+
+@app.route('/')
+def index():
+    stage = app.current_request.context['stage']
+    return { 'tokyo': f'/{stage}/loft/tokyo/events' }
+
+
+@app.route('/loft/tokyo/events')
+def feed():
+    host = app.current_request.headers['host']
+    stage = app.current_request.context['stage']
+    # Generate feed
+    fg = FeedGenerator()
+    fg.id(f'https://{host}/{stage}/loft/tokyo/events')
+    fg.title('Events feed for AWS Startup Loft')
+    fg.description('Events')
+    fg.link( href='https://aws-startup-lofts.com/apj/loft/tokyo/events' )
+    fg.language('ja')
+
+    for e in requests.get(INTERNAL_EVENT_URL).json()['future']:
+        if e.get('status', '') == 'live' and e.get('physicalLoft', '') == 'tokyo-loft':
+            event_id = e['id']
+            event_title = f"[{e['startDate']}] {e['title']}"
+            # Add entry
+            fe = fg.add_entry()
+            fe.id(event_id)
+            fe.title(event_title)
+            fe.description(e['summary'])
+            fe.link( href=f'https://aws-startup-lofts.com/apj/event/{event_id}' )
+            fe.pubDate(e['createdDate'])
+            fe.category( term=e['type'] )
+            fe.author( name=e['presenter'] )
+    for e in requests.get(EXTERNAL_EVENT_URL).json()['future']:
+        if e.get('status', '') == 'live' and e.get('physicalLoft', '') == 'tokyo-loft':
+            event_id = e['id']
+            event_title = f"[{e['startDate']}] {e['title']}"
+            # Add entry
+            fe = fg.add_entry()
+            fe.id(event_id)
+            fe.title(event_title)
+            fe.description(e['summary'])
+            fe.link( href=f'https://aws-startup-lofts.com/apj/external-event/{event_id}' )
+            fe.pubDate(e['createdDate'])
+            fe.category( term=e['type'] )
+            fe.author( name=e['presenter'] )
+    #feed  = fg.atom_str(pretty=True).decode('utf-8')
+    feed  = fg.rss_str(pretty=True).decode('utf-8')
+    return feed
